48f3e7562c5921e5924ee699f1f56daa0d4e9ee1
Vidhya Krishnaswamy <vidhyaworking24x7@gmail.com>
2026-07-21 13:21:05 -0500

    Amazon Price Checker Project

---
+++ exercise_2.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+url="https://www.amazon.com/Instant-Pot-Plus-Programmable-Sterilizer/dp/B075CYMYK6?th=1"
+header = {
+    "User-Agent": "CCBot/2.0 (https://commoncrawl.org/faq/)",
+    "Accept-Language": "en-US,en;q=0.5"
+}
+
+response = requests.get(url,headers=header)
+web_page=response.text
+
+soup=BeautifulSoup(web_page,"html.parser")
+
+price=soup.find(name="span",class_="aok-offscreen")
+amazon_price=price.get_text()
+print(amazon_price)
+
+product=soup.find(name="span",id="productTitle").getText().split("\n")[0]
+print(product)
+
+# if amazon_price<100:
+#     email="***@gmail.com"
+#     password="***"
+#
+#     with smtplib.SMTP("smtp.gmail.com",587) as server:
+#         server.starttls()
+#         server.login(email,password)
+#         server.sendmail(email,email,f"Subject: {product} price drop\n Amazon price dropped for {product}below target price. Buy now. Click below {url}".encode("utf-8"))
+#
+#
+
+
