d1c626884d5f0c6049551872b1d23f515d33fa07
Vidhya Krishnaswamy <vidhyaworking24x7@gmail.com>
2026-07-21 13:17:30 -0500

    Automatic Birthday Wisher Project

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-import requests
-from bs4 import BeautifulSoup
+##################### Normal Starting Project ######################
 import smtplib
+import datetime as dt
+import random
+import pandas as pd
 
-url="https://www.amazon.com/Instant-Pot-Plus-Programmable-Sterilizer/dp/B075CYMYK6?th=1"
-header = {
-    "User-Agent": "CCBot/2.0 (https://commoncrawl.org/faq/)",
-    "Accept-Language": "en-US,en;q=0.5"
-}
+# 2. Check if today matches a birthday in the birthdays.csv
+# HINT 1: Create a tuple from today's month and day using datetime. e.g.
+# today = (today_month, today_day)
+data = pd.read_csv("birthdays.csv")
+birthday_dict={(data["month"],data["day"]): (data["name"],data["email"],data["year"],data["month"],data["day"]) for (index, data) in data.iterrows()}
 
-response = requests.get(url,headers=header)
-web_page=response.text
+print(birthday_dict)
 
-soup=BeautifulSoup(web_page,"html.parser")
+current_date = dt.datetime.today()
+today_month = current_date.month
+today_day = current_date.day
+today=(today_month,today_day)
+birthday_person=[]
+if today in birthday_dict:
+    with open("C:/Users/krish/PycharmProjects/PythonProject/birthday-wisher-normal-start/letter_templates/letter_1.txt","r") as letter_1:
+        format_1=letter_1.read()
+    with open("C:/Users/krish/PycharmProjects/PythonProject/birthday-wisher-normal-start/letter_templates/letter_2.txt","r") as letter_2:
+        format_2=letter_2.read()
+    with open("C:/Users/krish/PycharmProjects/PythonProject/birthday-wisher-normal-start/letter_templates/letter_3.txt","r") as letter_3:
+        format_3=letter_3.read()
 
-price=soup.find(name="span",class_="aok-offscreen")
-amazon_price=price.get_text()
-print(amazon_price)
+    birthday_person=birthday_dict[today]
+    name=birthday_person[0]
+    email=birthday_person[1]
 
-product=soup.find(name="span",id="productTitle").getText().split("\n")[0]
-print(product)
+    letters=[format_1,format_2,format_3]
+    random_letter = random.choice(letters)
+    final_letter=random_letter.replace("[NAME]",name)
+    print(final_letter)
 
-# if amazon_price<100:
-#     email="***@gmail.com"
-#     password="***"
-#
-#     with smtplib.SMTP("smtp.gmail.com",587) as server:
-#         server.starttls()
-#         server.login(email,password)
-#         server.sendmail(email,email,f"Subject: {product} price drop\n Amazon price dropped for {product}below target price. Buy now. Click below {url}".encode("utf-8"))
-#
-#
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# HINT 2: Remember to call .starttls()
+# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
+# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+my_email = "***@gmail.com"
+password="***"
+
+with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+    connection.starttls()
+    connection.login(my_email,password)
+    connection.sendmail(from_addr=my_email,to_addrs=email,msg=f"Subject: Happy Birthday! \n\n {final_letter}".encode("utf-8"))
 
 
